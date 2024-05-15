4fffe2db679f4621becdcc8bcc8eab1a0d9d14d8
franck <carnet.franck.paul@gmail.com>
2024-05-15 10:48:10 +0200

    l10n

---
+++ locales/en/main.lang.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['What makes two plus two?'] = '';
+L10n::$locales['4'] = '';
+L10n::$locales['This is an accessible captcha.'] = '';
+L10n::$locales['Question has been successfully added.'] = '';
+L10n::$locales['Questions have been successfully removed.'] = '';
+L10n::$locales['Questions list has been successfully reinitialized.'] = '';
+L10n::$locales['Add a question'] = '';
+L10n::$locales['Question to add:'] = '';
+L10n::$locales['Answer:'] = '';
+L10n::$locales['Question list'] = '';
+L10n::$locales['Question:'] = '';
+L10n::$locales['Delete selected questions'] = '';
+L10n::$locales['Reset the list'] = '';
+L10n::$locales['To disable this plugin, you need to disable it %sfrom the plugins page%s.'] = '';
